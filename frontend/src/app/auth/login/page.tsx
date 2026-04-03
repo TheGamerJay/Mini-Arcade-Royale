18e367e1881e497645fb998c0054effbc497a85a
Mini Arcade Developer <dev@mini-arcade-royale.com>
2026-04-03 18:16:48 -0400

    Fix ESLint: Escape apostrophe in login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -105,7 +105,7 @@ export default function LoginPage() {
 
         <div className="mt-6 space-y-3">
           <p className="text-center text-gray-400">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/auth/register" className="text-arcade-primary hover:underline">
               Create One
             </Link>
